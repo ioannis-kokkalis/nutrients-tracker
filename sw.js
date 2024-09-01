307d4de26efc55fadcc960a5210f856075fbc76d
Ioannis Kokkalis <ioanniskokkalis2001@gmail.com>
2024-09-01 09:54:25 +0300

    rushed proof of conecept

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('nutriens_proof_of_concept').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
